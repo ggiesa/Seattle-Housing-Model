349b1806a35752e5b6aa381c50b4364805b7df0e
ggiesa <ggiesawilson@gmail.com>
2017-10-23 15:08:57 +0700

    Completed preliminary cleaning/engineering
    - Corrected for skew
    - Normalized
    - Saved data
    - Made additional plots to visualize plots made

--- script.py
+++ script.py
@@ -20,19 +20,21 @@ data = data.sort_values('date')
 # Creating new index, ordered by date
 data = data.reset_index(drop=True)
 
-# Define target variable, drop features that won't be used
-y = data.price
-compdata = data.drop(['id', 'date'], axis = 1)
-
-## The magnitudes of most of the features are very different from one another, so normalize right away
-#for column in compdata:
-#    compdata[column] = (compdata[column] - compdata[column].mean())/compdata[column].std()
+# Drop features that won't be used
+compdata = data.drop(['id', 'date', 'long'], axis = 1) # 'long' is just one value all throughout the dataset, so it's useless in this model
 
 # Checking for null values:
 nulls = sum(data.isnull().sum()) # No missing values!
 
+# Checking for negative values:
+for column in compdata:
+    if np.sum(compdata[column] < 0) > 0:
+        print('Negative values exist in %s' %(column))
+# No negative values!
+
  
-#%% Plotting for an overview of the data: distribution and correlation plots to check for skew and multicollinearity 
+#%% 
+''' Plotting for an overview of the data: distribution and correlation plots to check for skew and multicollinearity '''
 
 # First, a correlation matrix
 corr = compdata.corr()
@@ -40,22 +42,19 @@ sb.heatmap(corr, annot=True)
 plot.xticks(rotation = 45)
 plot.yticks(rotation = 0)
 
-#%%
-'''
-It looks like the dataset is fairly well behaved in terms of correlation between the independent variables, but there are some features 
-that are very poorly correlated to the sale price. Some of these poorly correlated features are surprising, like zipcode for example.
-Lets take a closer look at them vs. price
-'''
+#It looks like the dataset is fairly well behaved in terms of multicorrilarity, but it looks like 'sqft_above' and 'sqft_living' 
+#are so closely correlated that it's probably best to drop one of them. I'll arbitrarily choose 'sqft_above' to drop.
+compdata = compdata.drop('sqft_above', axis = 1)
 
-investigate = ['sqft_lot', 'condition', 'yr_built', 'zipcode', 'long']
-
-# Plot the features in question vs. price
-f1, ax1 = plot.subplots(2,3)
-ax1 = ax1.ravel()
-
-for i in range(len(investigate)):
-    ax1[i].scatter(data[investigate[i]], y, s=1, c='k')
-    ax1[i].set_title(investigate[i])
+#investigate = ['sqft_lot', 'condition', 'yr_built', 'zipcode', 'long']
+#
+## Plot the features in question vs. price
+#f1, ax1 = plot.subplots(2,3)
+#ax1 = ax1.ravel()
+#
+#for i in range(len(investigate)):
+#    ax1[i].scatter(data[investigate[i]], y, s=1, c='k')
+#    ax1[i].set_title(investigate[i])
 
 #%%
 ''' Engineering some features '''
@@ -69,7 +68,7 @@ zips, inverse = np.unique(tempzip, return_inverse = True)
 # Making an array with the mean price per zipcode 
 zipmeans = np.ones(len(zips))
 for i in range(len(zips)):
-    tempy = copy.copy(y)
+    tempy = copy.copy(compdata.price)
     zipmask = 1*(tempzip == zips[i])
     tempy *= zipmask
     zipmeans[i] = tempy[tempy!=0].mean()
@@ -86,12 +85,12 @@ compdata = compdata.rename(columns = {'zipcode':'meanzip'})
 # Make a plot of the new feature
 f2 = plot.figure() 
 ax2 = f2.add_subplot(111)
-ax2.scatter(compdata.meanzip, y, s=1, c='k')
+ax2.scatter(compdata.meanzip, compdata.price, s=1, c='k')
 ax2.axes.set_title('meanzip')
 
 # For now, drop the other poorly correlated features
-drop = ['sqft_lot', 'condition', 'yr_built', 'long']
-compdata = compdata.drop(drop, axis=1)
+#drop = ['sqft_lot', 'condition', 'yr_built', 'long']
+#compdata = compdata.drop(drop, axis=1)
 
 # Make a new correlation heatmap to check on things
 corr = compdata.corr()
@@ -99,25 +98,58 @@ sb.heatmap(corr, annot = True)
 plot.xticks(rotation = 45)
 plot.yticks(rotation = 0)
 
+#%% 
 
-#%% Creating a distribution plot
+''' Features are selected and ready for the next step of preprocessing. '''
+
+## Features are very different in terms of scale, so time for feature scaling. I'm choosing to do mean-normalization.
+#for column in compdata:
+#    compdata[column] = (compdata[column] - compdata[column].mean())/(compdata[column].max() - compdata[column].min())
+
 
 # Making a list of features to label the plot with, and a matrix from compdata for plotting
 features = list(compdata.axes[1])
 plotdata = np.asmatrix(compdata)
 
 # Making the distribution plot
-f, ax  = plot.subplots(3,5)
-ax = ax.ravel()
+f3, ax3  = plot.subplots(3,6)
+ax3 = ax3.ravel()
 
 for i in range(len(features)):
-    sb.distplot(plotdata[:,i], ax = ax[i])
-    ax[i].set_title(features[i])
+    sb.distplot(plotdata[:,i], ax = ax3[i])
+    ax3[i].set_title(features[i])
 
-# Looks like 
+# There is significant right skew in most of the variables, and left skew in a couple, so it's probably worth normalizing them.
+right_normalize = ['price', 'bedrooms', 'bathrooms', 'sqft_living', 'sqft_basement', 'meanzip', 'sqft_living15', 'floors']
+left_normalize = ['lat', 'yr_built', ]
 
-    
-#
+# log+1 transformation to correct for right skew
+for feature in right_normalize:
+    compdata[feature] = np.log1p(compdata[feature])
+
+# Power transformations to correct for left skew
+compdata.lat = compdata.lat**7
+compdata.yr_built = compdata.yr_built**5
+
+# Plot again to visualize transformation
+plotdata = np.asmatrix(compdata)
+f4, ax4  = plot.subplots(3,6)
+ax4 = ax4.ravel()
+
+for i in range(len(features)):
+    sb.distplot(plotdata[:,i], ax = ax4[i])
+    ax4[i].set_title(features[i])
+# Features look much better.
+
+# Features are very different in terms of scale, so time for feature scaling. I'm choosing to do mean-normalization.
+for column in compdata:
+    compdata[column] = (compdata[column] - compdata[column].mean())/(compdata[column].max() - compdata[column].min())
+#%% 
+''' I think the data is ready for a preliminary model. Time to split the data into training and CV sets '''    
+
+# Saving to CSV
+compdata.to_csv('data.csv', index = False)
+
 ## Because this is time series data, I use TimeSeriesSplit to obtain indices of training and CV sets (k-fold for time series)
 #tscv = TimeSeriesSplit(n_splits = 10)
 #
